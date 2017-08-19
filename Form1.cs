34ea1ab7c47be4d899de1e15586c455e2755aae9
Adarsh Pradyut <31154287+AdarshPradyut@users.noreply.github.com>
2017-08-19 14:23:41 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SendThis;
+
+namespace dll_testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SendThis.SendThis st = new SendThis.SendThis();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = 0;
+                while (i < Convert.ToInt32(textBox1.Text))
+                {
+                    st.start("text" + i);
+                    Console.WriteLine("text" + i);
+                    i++;
+                }
+            }
+            catch(Exception exp)
+            {
+
+            }
+        }
+    }
+}
