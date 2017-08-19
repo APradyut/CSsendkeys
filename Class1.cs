265ef48e317ecaf3722a6c715deb8336d5b46c8e
AdarshPradyut <31154287+AdarshPradyut@users.noreply.github.com>
2017-08-19 14:11:30 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Forms;
+public class SendThis
+{
+    private static string text;
+    public static void start(string input)
+    {
+        setText(input);
+        sendthis();
+    }
+    private static void setText(string input)
+    {
+        if (input == "Enter" || input == "enter")
+        {
+            text = "{Enter}";
+            return;
+        }
+        switch (input)
+        {
+            case "up":
+                text = "{UP}";
+                break;
+
+            case "down":
+                text = "{DOWN}";
+                break;
+
+            case "right":
+                text = "{RIGHT}";
+                break;
+
+            case "left":
+                text = "{LEFT}";
+                break;
+            default:
+                text = input;
+                break;
+        }
+    }
+    private static void sendthis()
+    {
+        SendKeys.Send(text);
+    }
+}
